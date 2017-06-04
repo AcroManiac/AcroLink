5bff0e695badde110f5dae194ec6498867e410a5
AcroManiac <artem.orlov@gmail.com>
2017-06-04 22:34:55 +0300

    Added relative models for user profile

--- Server/DjangoServer/DjangoServer/settings.py
+++ Server/DjangoServer/DjangoServer/settings.py
@@ -86,8 +86,6 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'UserProfile.User'
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
--- Server/DjangoServer/UserProfile/models.py
+++ Server/DjangoServer/UserProfile/models.py
@@ -2,22 +2,70 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
-class UserLevel(models.Model):
-	name = models.TextField(max_length=20, default='Non-specified')
+class Level(models.Model):
+	name = models.TextField(max_length=20, default='Beginner')
 
-class User(AbstractUser):
-    phone = models.TextField(max_length=20, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    practice_start_date = models.DateField(null=True, blank=True)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+	def __unicode__(self):
+		return self.name
 
-    level = models.OneToOneField(UserLevel, null=True)
-    # status = models.OneToOneField(Status)
-    # country = models.OneToOneField(Country)
+
+class Position(models.Model):
+	profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+	name = models.TextField(max_length=20, default='Base')
+
+	def __unicode__(self):
+		return self.name
+		
+
+class Role(models.Model):
+	profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+	name = models.TextField(max_length=20, default='Student')
+
+	def __unicode__(self):
+		return self.name
+		
+
+class Country(models.Model):
+	code = models.TextField(max_length=2)
+	name = models.TextField(max_length=45)
+
+	def __unicode__(self):
+		return self.name
+
+
+class SocialNetwork(models.Model):
+	name = models.TextField(max_length=20)
+	url = models.URLField(null=True)
+	logo_url = models.URLField(null=True)
+
+	def __unicode__(self):
+		return self.name
+
+class SocialNetworkLink(models.Model):
+	profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+	social_network = models.ForeignKey('SocialNetwork', on_delete=models.CASCADE)
+	url = models.URLField(null=True)
+
+	def __unicode__(self):
+		return self.url
+		
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	phone = models.TextField(max_length=20, blank=True)
+	birth_date = models.DateField(null=True, blank=True)
+	practice_start_date = models.DateField(null=True, blank=True)
+	bio = models.TextField(max_length=500, blank=True)
+	location = models.CharField(max_length=30, blank=True)
+	avatar = models.ImageField(upload_to='avatars/', null=True)
+
+	# position = models.ManyToOneField(Position, null=True)
+	# role = models.ManyToOneField(Role, null=True)
+	level = models.OneToOneField('Level', null=True)
+	country = models.OneToOneField('Country', null=True)
+	social_network = models.ManyToManyField('SocialNetwork', through='SocialNetworkLink')
