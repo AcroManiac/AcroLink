06c10bed4a73e9108dee55db5ca43ccadfa47f5d
AcroManiac <artem.orlov@gmail.com>
2017-06-09 19:56:25 +0300

    Added serializer for relative user table

--- Server/DjangoServer/DjangoServer/main/serializers.py
+++ Server/DjangoServer/DjangoServer/main/serializers.py
@@ -2,8 +2,16 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from models import Profile
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
 class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only=True)
 
     class Meta:
         model = Profile
-        fields = ('id', 'phone', 'birth_date', 'practice_start_date', 'bio', 'location', 'avatar')
+        fields = ('id', 'phone', 'birth_date', 'practice_start_date', 'bio', 'location', 'avatar', 'user')
