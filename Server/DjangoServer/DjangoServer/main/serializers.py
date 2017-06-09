b75cf006a4a4a8694513f36ecddcd62c09e4a14b
AcroManiac <artem.orlov@gmail.com>
2017-06-09 19:25:12 +0300

    Fixed profile routing

--- Server/DjangoServer/DjangoServer/main/serializers.py
+++ Server/DjangoServer/DjangoServer/main/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from main.models import Profile
+from models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
 
