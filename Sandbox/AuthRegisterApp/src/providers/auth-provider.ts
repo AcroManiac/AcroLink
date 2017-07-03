b15ba9d93895eb211ae4de927da268ac4fc69239
AcroManiac <artem.orlov@gmail.com>
2017-07-03 18:34:53 +0300

    Improved Http Observables

--- Sandbox/AuthRegisterApp/src/providers/auth-provider.ts
+++ Sandbox/AuthRegisterApp/src/providers/auth-provider.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { Storage } from '@ionic/storage';
+import { AlertController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 
 /*
@@ -14,8 +15,11 @@ export class AuthProvider {
 
   public token: any;
 
-  constructor(public http: Http, private storage: Storage) {
-    console.log('Hello AuthProvider Provider');
+  constructor(
+  	public http: Http,
+  	private storage: Storage,
+  	private alertCtrl: AlertController) {
+  		console.log('Hello AuthProvider Provider');
   }
 
   checkAuthentication(){
@@ -47,49 +51,49 @@ export class AuthProvider {
 
   	console.log("createAccount(): Details: " + JSON.stringify(details));
 
-  	return new Promise((resolve, reject) => {
+	let headers = new Headers();
+	headers.append('Content-Type', 'application/json');
 
-	    let headers = new Headers();
-	    headers.append('Content-Type', 'application/json');
+	this.http.post('http://127.0.0.1:8000/api/v1/rest-auth/registration/', JSON.stringify(details), {headers: headers})
+		.map(res => res.json())
+	  	.subscribe((data) => {
 
-	    this.http.post('http://127.0.0.1:8000/api/v1/rest-auth/registration/', JSON.stringify(details), {headers: headers})
-	      .subscribe(res => {
+	  	this.token = data.token;
+	    this.storage.set('token', data.token);
 
-	      	let data = res.json();
-	      	this.token = data.token;
-	        this.storage.set('token', data.token);
-	        resolve(data);
-
-	      }, (err) => {
-	      	console.log("createAccount(): Details: " + JSON.stringify(err));
-	      	reject(err);
-	      });
-
-  	});
+	  }, (err) => {
+	  	console.log("createAccount(): Error: " + JSON.stringify(err));
+	  	
+	  	let alert = this.alertCtrl.create({
+			title: 'Register Error',
+			subTitle: JSON.stringify(err),
+			buttons: ['OK'] });
+		alert.present();
+	  });
 
   }
 
   login(credentials){
 
-  	return new Promise((resolve, reject) => {
+	let headers = new Headers();
+	headers.append('Content-Type', 'application/json');
 
-	    let headers = new Headers();
-	    headers.append('Content-Type', 'application/json');
+	this.http.post('http://127.0.0.1:8000/api/v1/rest-auth/login/', JSON.stringify(credentials), {headers: headers})
+	  .map(res => res.json())
+	  .subscribe((data) => {
 
-	    this.http.post('http://127.0.0.1:8000/api/v1/rest-auth/login/', JSON.stringify(credentials), {headers: headers})
-	      .subscribe(res => {
+	  	this.token = data.token;
+	    this.storage.set('token', data.token);
 
-	      	let data = res.json();
-	      	this.token = data.token;
-	        this.storage.set('token', data.token);
-	        resolve(data);
-
-	        resolve(res.json());
-	      }, (err) => {
-	      	reject(err);
-	      });
-
-  	});
+	  }, (err) => {
+	  	console.log("login(): Error: " + JSON.stringify(err));
+	  	
+	  	let alert = this.alertCtrl.create({
+			title: 'Login Error',
+			subTitle: JSON.stringify(err),
+			buttons: ['OK'] });
+		alert.present();
+	  });
 
   }
 
