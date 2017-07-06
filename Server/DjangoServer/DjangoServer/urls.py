42f0b39b240aed467ae90f7ac5f724e17ed4ae9b
AcroManiac <artem.orlov@gmail.com>
2017-07-06 18:58:56 +0300

    Improved confirmation email

--- Server/DjangoServer/DjangoServer/urls.py
+++ Server/DjangoServer/DjangoServer/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     url(r'^api/v1/admin/', include(admin.site.urls)),
     url(r'^api/v1/rest-auth/', include('rest_auth.urls')),
     url(r'^api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
-    url(r'^account-confirm-email/(?P<key>[-:\w]+)/$', allauthemailconfirmation, name='account_confirm_email'),
     url(r'^api/v1/accounts/', include('allauth.urls')),
     url(r'^api/v1/accounts/profile/$', RedirectView.as_view(url='/', permanent=True), name='profile-redirect'),
 
