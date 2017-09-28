fa02605385327fbe516d53e811a4ee460a2bb40c
AcroManiac <artem.orlov@gmail.com>
2017-09-28 09:01:34 +0300

    Added make DB script for Python 3

---
+++ Server/DjangoServer/MakeDatabase3.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+
+python3 manage.py makemigrations main
+python3 manage.py makemigrations reference
+
+python3 manage.py migrate
+
+python3 manage.py loaddata DjangoServer/reference/fixtures/Country.json
+python3 manage.py loaddata DjangoServer/reference/fixtures/Level.json
+python3 manage.py loaddata DjangoServer/reference/fixtures/Position.json
+python3 manage.py loaddata DjangoServer/reference/fixtures/Role.json
+python3 manage.py loaddata DjangoServer/reference/fixtures/SocialNetwork.json
+
+#python3 manage.py loaddata DjangoServer/main/fixtures/User.json
+#python3 manage.py loaddata DjangoServer/main/fixtures/Profile.json
