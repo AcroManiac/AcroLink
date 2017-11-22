0aeb875c75869ea4fdf36d513b2b517b075c0641
Artem Orlov <a.orlov@veedo.ru>
2017-11-22 17:07:52 +0300

    Improved data models

--- Server/DjangoServer/DjangoServer/main/models.py
+++ Server/DjangoServer/DjangoServer/main/models.py
@@ -8,13 +8,25 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
-class SocialNetworkLink(models.Model):
-	profile = models.ForeignKey('main.Profile', on_delete=models.CASCADE)
-	social_network = models.ForeignKey('reference.SocialNetwork', on_delete=models.CASCADE)
-	url = models.URLField(null=True)
+# class SocialNetworkLink(models.Model):
+# 	profile = models.ForeignKey('main.Profile', on_delete=models.CASCADE)
+# 	social_network = models.ForeignKey('reference.SocialNetwork', on_delete=models.CASCADE)
+# 	url = models.URLField(null=True)
 
-	def __unicode__(self):
-		return self.url
+# 	def __unicode__(self):
+# 		return self.url
+
+class Location(models.Model):
+	street_number = models.TextField(max_length=10, blank=True)
+	route = models.TextField(max_length=64, blank=True)
+	locality = models.TextField(max_length=32, blank=True)
+	state = models.TextField(max_length=32, blank=True)
+	country = models.TextField(max_length=64, blank=True)
+	postal_code = models.TextField(max_length=10, blank=True)
+	country_code = models.TextField(max_length=4, blank=True)
+	latitude = models.TextField(max_length=32, blank=True)
+	longtitude = models.TextField(max_length=32, blank=True)
+	placeId = models.TextField(max_length=256, blank=True)
 
 
 class Profile(models.Model):
@@ -23,16 +35,15 @@ class Profile(models.Model):
 	birth_date = models.DateField(null=True, blank=True)
 	practice_start_date = models.DateField(null=True, blank=True)
 	bio = models.TextField(max_length=500, blank=True)
-	location = models.CharField(max_length=30, blank=True)
 	avatar = models.ImageField(upload_to='avatars/', null=True)
-	score = models.IntegerField(null=False, blank=False, default=0)
+	karma = models.IntegerField(null=False, blank=False, default=0)
+
+	location = models.OneToOneField('Location', on_delete=models.CASCADE)
 
 	level = models.ForeignKey('reference.Level', null=True)
-	country = models.ForeignKey('reference.Country', null=True)
-
 	position = models.ManyToManyField('reference.Position')
 	role = models.ManyToManyField('reference.Role')
-	social_network = models.ManyToManyField('reference.SocialNetwork', through='main.SocialNetworkLink')
+	# social_network = models.ManyToManyField('reference.SocialNetwork', through='main.SocialNetworkLink')
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
