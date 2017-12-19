b41b7412d80e027e97893896907bbd1b8b36269a
Artem Orlov <a.orlov@veedo.ru>
2017-12-19 15:44:03 +0300

    Added AutoOneToOneField for location creation

--- Server/DjangoServer/DjangoServer/main/models.py
+++ Server/DjangoServer/DjangoServer/main/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from annoying.fields import AutoOneToOneField
 
 # Create your models here.
 
@@ -39,7 +40,7 @@ class Profile(models.Model):
 	avatar = models.ImageField(upload_to='avatars/', blank=True)
 	score = models.IntegerField(null=False, blank=False, default=0)
 
-	location = models.ForeignKey('Location', null=True, blank=True, on_delete=models.CASCADE)
+	location = AutoOneToOneField('Location', null=True, on_delete=models.CASCADE)
 
 	level = models.ForeignKey('reference.Level', null=True, blank=True, on_delete=models.CASCADE)
 	position = models.ManyToManyField('reference.Position', blank=True)
--- Server/DjangoServer/DjangoServer/settings.py
+++ Server/DjangoServer/DjangoServer/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'corsheaders',
 
+    'annoying',
     # 'rolepermissions',
 
     'DjangoServer.main',
